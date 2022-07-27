5122cf8ac8fed346eabf6435301cd80e2656f158
Aayush Rajthala <Aayushrajthala@gmail.com>
2022-07-27 16:55:08 +0545

    Added View Restrictions

--- migrations/1_user/1-create-user-table.sql
+++ migrations/1_user/1-create-user-table.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 21, 2022 at 07:11 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: Jul 26, 2022 at 12:59 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -25,11 +25,14 @@ CREATE TABLE `user` (
   `id` int(11) UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `dob` date NOT NULL,
-  `gender` int(1) NOT NULL COMMENT '0 = Male\r\n1 = Female\r\n2 = Others',
-  `address` varchar(255) NOT NULL,
-  `mobile` varchar(10) NOT NULL,
-  `citizenship` varchar(11) NOT NULL,
+  `dob` date DEFAULT NULL,
+  `gender` int(1) DEFAULT NULL COMMENT '0 = Male\r\n1 = Female\r\n2 = Others',
+  `address` varchar(255) DEFAULT NULL,
+  `mobile` varchar(10) DEFAULT NULL,
+  `citizenship` varchar(11) DEFAULT NULL,
+  `haslicense` tinyint(1) DEFAULT NULL COMMENT '1= "Yes", 0 = "No"',
+  `license` varchar(13) DEFAULT NULL,
+  `filledprofile` tinyint(1) DEFAULT NULL COMMENT '1= "Yes", 0 = "No"',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `deleted_at` timestamp NULL DEFAULT NULL
