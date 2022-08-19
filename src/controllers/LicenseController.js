182c4b48a714a7567fc004618b50518798a23de0
Aayush Rajthala <Aayushrajthala@gmail.com>
2022-08-19 10:06:45 +0545

    Modified License View

--- src/controllers/LicenseController.js
+++ src/controllers/LicenseController.js
@@ -10,10 +10,18 @@ async function index(req, res) {
     try {
         let email = req.session.user;
         const userInfo = await getUserInfo(email);
+        if (userInfo.status & userInfo.result.length != 0) {
+            if (userInfo.result[0].haslicense == '1') {
+                res.render('license/index', {
+                    userInfo: userInfo.result[0],
+                });
+            } else {
+                res.render('error', {
+                    error: 'License Information Not Found!'
+                });
+            }
+        }
 
-        res.render('license/index', {
-            userInfo: userInfo.result[0],
-        });
     } catch (error) {
         logger.error(`LICENSE VIEW ERROR: ${error}`);
         res.render("error", {
